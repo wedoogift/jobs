c1ed5d6a320a3b7a0f6fc75da793c2374782dea8
Victor Levasseur <victorlevasseur52@gmail.com>
2018-05-04 17:59:28 +0200

    Manage the errors in the calculator-server

--- Frontend/calculator-server/app.js
+++ Frontend/calculator-server/app.js
@@ -32,7 +32,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({ status: err.status || 500, message: 'Service not found!' });
 });
 
 module.exports = app;
